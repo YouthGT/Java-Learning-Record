da868a4857f5c5d0df670103908ca3f76105ac1d
youthgt <youthgt@foxmail.com>
2021-05-11 23:15:32 +0800

    break

---
+++ src/main/java/Test14break.java
@@ -0,0 +1,12 @@
+public class Test14break {
+    public static void main(String[] args) {
+        for (int i=0;i<=5;i++)
+        {
+            if(i==3)//注意双等于号是等于
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
