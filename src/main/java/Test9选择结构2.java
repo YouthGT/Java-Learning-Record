21dafbd760e77ac3f7a0307585b0bc8b8e8e063d
youthgt <youthgt@foxmail.com>
2021-05-11 11:07:23 +0800

    add switch struct

---
+++ src/main/java/Test9选择结构2.java
@@ -0,0 +1,20 @@
+public class Test9选择结构2 {
+    public static void main (String[] args){
+        /**
+         * switch结构
+         * 记得加break
+         */
+        int m=5;
+        switch (m)
+        {
+            case 1:
+                break;
+            case 5:
+                System.out.println("匹配");
+                break;
+            case 3:
+                System.out.println("不匹配");
+
+        }
+    }
+}
