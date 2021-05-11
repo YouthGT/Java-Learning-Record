d80a445244da467183942d84d9c12e8b35010415
youthgt <youthgt@foxmail.com>
2021-05-11 11:15:30 +0800

    add for struct

---
+++ src/main/java/Test10循环结构for.java
@@ -0,0 +1,12 @@
+public class Test10循环结构for {
+    public static void main(String[] args) {
+        /**for循环结构！！！重点用的多
+         * DRY:Don’t Repeat Youself
+         */
+        int i;
+        for (i=0;i<9;i++)
+        {
+            System.out.println("加油加油加油");
+        }
+    }
+}
