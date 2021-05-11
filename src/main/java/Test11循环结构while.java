70e1b1f6dc2017ba5887d306e0b686439199f194
youthgt <youthgt@foxmail.com>
2021-05-11 11:22:45 +0800

    add while

---
+++ src/main/java/Test11循环结构while.java
@@ -0,0 +1,14 @@
+public class Test11循环结构while {
+    public static void main(String[] args) {
+        /**
+         * while结构
+         */
+        int i = 0;
+        while (i < 3)
+        {
+            System.out.println("youthgt" + i);
+            i++;
+        }
+        System.out.println("over");
+    }
+}
