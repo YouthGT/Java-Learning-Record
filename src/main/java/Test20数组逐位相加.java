ae4f762688ad5db974b70120787e6e314cbfcd62
youthgt <youthgt@foxmail.com>
2021-05-12 20:12:21 +0800

    消除魔法数

--- src/main/java/Test20数组逐位相加.java
+++ src/main/java/Test20数组逐位相加.java
@@ -3,7 +3,7 @@ public class Test20数组逐位相加 {
     public static void main(String[] args) {
         int[]num1={3,8,9};
         int[]num2={5,2,8};
-        int[]num3=new int[3];
+        int[]num3=new int[num1.length];
         //for(int i=0;i<3;i++)
         for (int i=0;i<num3.length;i++)
         {
