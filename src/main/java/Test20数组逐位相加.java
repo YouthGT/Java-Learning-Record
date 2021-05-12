9ca1fbe75f2de644b554a6b7cd896720ccba4455
youthgt <youthgt@foxmail.com>
2021-05-12 20:09:29 +0800

    change array num method

--- src/main/java/Test20数组逐位相加.java
+++ src/main/java/Test20数组逐位相加.java
@@ -4,7 +4,8 @@ public class Test20数组逐位相加 {
         int[]num1={3,8,9};
         int[]num2={5,2,8};
         int[]num3=new int[3];
-        for(int i=0;i<3;i++)
+        //for(int i=0;i<3;i++)
+        for (int i=0;i<num3.length;i++)
         {
                 num3[i]=num1[i]+num2[i];
         }
