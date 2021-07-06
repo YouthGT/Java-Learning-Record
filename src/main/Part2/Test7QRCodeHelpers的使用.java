3f9f5bb0b90b34ffcc10c795e42b0a231d4e1d37
youthgt <youthgt@foxmail.com>
2021-07-06 23:04:50 +0800

    检测QRcode是否存在网址

--- src/main/Part2/Test7QRCodeHelpers的使用.java
+++ src/main/Part2/Test7QRCodeHelpers的使用.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
 
 public class Test7QRCodeHelpers的使用 {
     public static void main(String[] args) {
-        BufferedImage image = QRCodeHelpers.generateQRCodeImage("youthgt Test",200,200);//生成二维码 中文有问题
+        BufferedImage image = QRCodeHelpers.generateQRCodeImage("https://youthgt.github.io/",200,200);//生成二维码 中文有问题
         ImageHelpers.writeToFile(image,"png","F:\\AndroidStudioProjects\\java\\src\\main\\Java\\二维码测试\\1.png");//保存到文件
         Result result = QRCodeHelpers.parseImage("F:\\AndroidStudioProjects\\java\\src\\main\\Java\\二维码测试\\1.png");//扫描二维码得到结果
         System.out.println(result);//打印出扫描的结果
--- src/main/Part2/Test9检测二维码是否包含网址.java
+++ src/main/Part2/Test9检测二维码是否包含网址.java
@@ -1,3 +1,31 @@
+import com.google.zxing.Result;
+import com.yzk18.commons.IOHelpers;
+import com.yzk18.commons.QRCodeHelpers;
+import org.apache.commons.io.filefilter.IOFileFilter;
+
 public class Test9检测二维码是否包含网址 {
+    public static void main(String[] args) {
+        String [] files = IOHelpers.getFilesRecursively("F:\\AndroidStudioProjects\\java\\src\\main\\Java\\二维码测试","png","jpg","gif");
+        boolean qrcodeFoundSite = false;
+        for (String file:files)
+        {
+            Result result = QRCodeHelpers.parseImage(file);
+            if (result.getText().startsWith("http://")||result.getText().startsWith("https://"))
+            {
+                qrcodeFoundSite = true;
+                break;
+            }
+
+
+        }
+        if (qrcodeFoundSite)
+        {
+            System.out.println("有宣传性二维码图片");
+        }
+        else
+        {
+            System.out.println("检测通过");
+        }
+    }
 
 }
